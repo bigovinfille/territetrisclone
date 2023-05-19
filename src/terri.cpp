33e172bfa48a407a9c15c99fde6ae2b43bfa784a
bigovinfille <bigovinfille@gmail.com>
2023-05-19 13:35:18 -0700

    second commit

--- src/terri.cpp
+++ src/terri.cpp
@@ -1,4 +1,4 @@
-// Tetris clone 
+// Terri.cpp, tetris clone 
 // "Terri"
 // start 5/19/23
 // last edited 5/19/23
@@ -14,6 +14,8 @@ int main(void)
 
 	InitWindow(screenW, screenH, "TESTT!!");
 
+	Vector2 ballPosition = { (float)screenW/2, (float)screenH/2 };
+
 	//set fps... sets fps
 	SetTargetFPS(30);
 
@@ -21,16 +23,29 @@ int main(void)
 	//note: WindowShouldClose detects window close button or esc key enters!
 	while (!WindowShouldClose()) 
 	{
-		// to come
+		// update
+		// 
+		if (IsKeyDown(KEY_RIGHT)) 
+			ballPosition.x += 2.0f;
+		if (IsKeyDown(KEY_LEFT))
+			ballPosition.x -= 2.0f;
+		if (IsKeyDown(KEY_UP))
+			ballPosition.y -= 2.0f;
+		if (IsKeyDown(KEY_DOWN))
+			ballPosition.y += 2.0f;	
+		//
 
+		//draw
+		//
 		BeginDrawing();
 			
 			ClearBackground(RAYWHITE);
 
-			DrawText("My first window swag", 190, 200, 20, LIGHTGRAY);
+			DrawText ("move ball with arrow keys", 10, 10, 20, DARKGRAY);
 
+			DrawCircleV (ballPosition, 50, GREEN);
 		EndDrawing();
-
+		//
 
 	}
 	//clean up
